11e6b9c7b83a536e4a0e2500ef6f59eac25a4f80
OzoneMotion <seovelaz@gmail.com>
2024-01-11 17:13:59 -0600

    Ejercicios resueltos en su totalidad c:, se me olvidó quitar unos comentarios jsjs

--- ejercicios1101.js
+++ ejercicios1101.js
@@ -53,15 +53,6 @@ let resultado = counter(posArray)
 console.log(resultado)
 
 //Antipodal Average
- /*
-Given an array, return a shorter array following these steps:
-- Split the array into two equal parts*. If unequal, remove the middle element to obtain two equal arrays.
-- Sum each number of the first part with the inverse numbers of the second part.
-- Divide each number of the resulting array by 2.
-
-Example:
-- For the array `[1,2,3,5,22,6]`, the result should be `[3.5, 12, 4]`.*/
-
 const miiArray = [1, 2, 3, 4, 5, 6, 7];
 function antipodalAverage(array) {
     const halfLength =array.length / 2; //obtener la mitad
