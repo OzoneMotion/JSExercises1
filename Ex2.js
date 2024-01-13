ea9977cb624ba24ff8f2a23e47883676eb5f103a
OzoneMotion <seovelaz@gmail.com>
2024-01-12 19:06:18 -0600

    Terminado ejercicio de timer

--- Ex2.js
+++ Ex2.js
@@ -3,7 +3,8 @@ Write a program that prompts a user for their data: username, age, and a list of
 Store the information and then showcase it in the console. 
 Take note that the output for the films should add a small message like: 
 'The film {film} is one of my favorites'.
-*/ 
+*/
+/*
 let userName = window.prompt("Dime tu nombre")
 let userAge = window.prompt("¿Cuántos años tienes?")
 let moviesArray = [];
@@ -13,7 +14,7 @@ for (i = 0; i <=2; i++) {
   moviesArray.push(valor);
 }
 console.log(`Hola eres ${userName}, tienes ${userAge} años y una de mis películas favoritas es "${moviesArray[1]}".`)
-
+*/
 
 
 /*Highest number
@@ -38,21 +39,32 @@ Write a program that asks a user for the amount of seconds needed until an alarm
 (message) is executed alongside a text to show once those seconds have passed in real time.
 Result example: "Time for bed after 10 seconds".*/
 
+
+function timer() {
+    let secondString = prompt("Put your timer (in seconds): ") * 1000
+    let secondInt = parseInt(secondString, 10);
+    console.log(typeof (secondInt))
+
+    setTimeout(() => { alert(`Time for bed after  ${secondString / 1000}  seconds`); }, secondInt);
+}
+timer();
+
 /*Palindrome
 Write a program that prompts for a word or sentence (it can be capitalized, have spaces and punctuation). 
 Figure out if the sentence/word is a palindrome or not. Ignoring punctuation, spaces and capitalized letters.*/
-/*
+
 let palabra = prompt("Ingresa la palabra para evaluar si es un palíndromo:")
 function palindromo(palabra) {
     palabra = palabra.toLowerCase().replace(/\s/g, ''); // Convertir la palabra a minúsculas y quitar los espacios
     return palabra === palabra.split('').reverse().join(''); // Comparar la palabra original con su inversa
-  }
-  if (palindromo(palabra)) {
+}
+if (palindromo(palabra)) {
     console.log(palabra + ' es un palíndromo');
-  } else {
+} else {
     console.log(palabra + ' no es un palíndromo');
-  }
-/*
+}
+
+
 /*Factorial
 Write a program that prompts for an intenger number n. Where  1 <= n. Solve this using recursion.*/
 
